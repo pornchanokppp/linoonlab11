597c233a8ad0d66c4af75f64e754bda9b3ac6501
linoonlab12 <you@example.com>
2023-01-06 17:04:33 +0700

    11.2

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -6,13 +6,19 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	dest << "-------------------- BOOM ---------------------\n";
+	string text;
+	while (getline(source,text)){
+		dest << text << endl;
+	}
+	dest << "-------------------- HA!! ---------------------";
+	
+	
 
     source.close();
     dest.close();
 	return 0;
-}
+}}
